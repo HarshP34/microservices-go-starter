5c501fddc67d44b73d56c0dca2e4d9c6f441ad48
Harsh Patel <hupatel1005@gmail.com>
2026-03-14 13:00:54 +0530

    Revert "feat(rabbitmq): implement dead letter exchange and queue for message handling"
    This reverts commit 9b17e3f0e273c11f885ae46297e01b33e24f74d6.

--- services/payment-service/internal/events/trip_consumer.go
+++ services/payment-service/internal/events/trip_consumer.go
@@ -3,7 +3,6 @@ package events
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"log"
 
 	"ride-sharing/services/payment-service/internal/domain"
@@ -32,8 +31,6 @@ func (c *TripConsumer) Listen() error {
 			log.Printf("Failed to unmarshal message: %v", err)
 			return err
 		}
-
-		return fmt.Errorf("Simulated error for trip event")
 
 		var payload messaging.PaymentTripResponseData
 		if err := json.Unmarshal(message.Data, &payload); err != nil {
